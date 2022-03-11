ff1d5ee7be1944e9599764b60b0e2f3047c99f30
lindmon <m.eryzhenskaia@gmail.com>
2022-03-11 15:48:51 +0100

    just commit

--- app.js
+++ app.js
@@ -2,26 +2,5 @@
 
 
 
-// const button = document.querySelector('button');
-// const header = document.querySelector('h1');
 
-// button.addEventListener('click', () => {
-//     fetch("https://api.adviceslip.com/advice")
-// .then(result => result.json())
-// .then(data => {
-//     header.innerText = data.slip.advice;
-// });
-// });
 
-fetch(“https://mockbin.com/request”)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
