6cd26220cc38fc53f7d9e8f12e5f39d88aac9b15
lindmon <m.eryzhenskaia@gmail.com>
2022-03-19 10:26:20 +0100

    animation added: slides cursor

--- app.js
+++ app.js
@@ -1,6 +1,87 @@
+let controller;
+let slideScene;
+let pageScene;
 
+function animateSlides(){
+    //Init Controler
+    controller = new ScrollMagic.Controller();
+    // Select some things
+    const sliders = document.querySelectorAll('.slide');
+    const nav = document.querySelector('.nav-header');
+    sliders.forEach((slide, index, slides) => {
+        const revealImg = slide.querySelector('.reveal-img');
+        const img = slide.querySelector('img');
+        const revealText = slide.querySelector('.reveal-text');
+        // GSAP
+        //we use TIMELINE by introducing the new vairable slideTL, after slideTL.fromTo
+        const slideTl = gsap.timeline({
+            defaults:{duration: 1, ease: "power2.inOut"}
+        });
+        slideTl.fromTo(revealImg, { x: "0%"}, { x: "100%"});
+        slideTl.fromTo(img, {scale: 2}, {scale: 1}, '-=1');
+        slideTl.fromTo(revealText,{x: "0%"}, {x:"100%"}, "-=0.75");
+        slideTl.fromTo(nav, {y: "-100%"}, {y:"0%"}, "-=0.5");
+        //added the animation to the scroller
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse:false
+        })
+            .setTween(slideTl)
+            .addIndicators()
+            .addTo(controller)
+            //New animation
+            const pageTl = gsap.timeline();
+            let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+            pageTl.fromTo(nextSlide, {y:"0%"}, {y:"50%"});
+            pageTl.fromTo(slide, {opacity:1, scale:1}, {opacity:0, scale:0});
+            pageTl.fromTo(nextSlide, {y:"50%"}, {y:"0%"}, '-=0.5');
+            //Create new scene (new variable - new triggers-new hooks, indicators and controllers)
+            pageScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                //duration - it ends in the end of the slide(or another trigger) while scrolling
+                duration:"100%",
+                triggerHook: 0
+            })
+            .setTween(pageTl)
+            .setPin(slide, {pushFollowers: false})
+            .addIndicators({colorStart:"white", colorTrigger:"white", name:"page", indent:200})
+            .addTo(controller)
+    });
+}
 
+//create cursor animation (grab the cursor, change the position to the position of the cursor)
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector('span');
 
+function cursor(e){
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e){
+    const item = e.target;
+    if(item.id === "logo" || item.classList.contains('burger')){
+        mouse.classList.add("nav-active");
+    } else {
+        mouse.classList.remove("nav-active");
+    }
+    if(item.classList.contains("explore")){
+        mouse.classList.add("explore-active");
+        gsap.to(".title-swipe", 1, { y: "0%"});
+        mouseTxt.innerText = 'Tap';
+    }else{
+        mouse.classList.remove("explore-active");
+        mouseTxt.innerText = ' ';
+        gsap.to(".title-swipe", 1, { y: "100%"});
+    }
+}
+
+window.addEventListener('mousemove', cursor );
+//change the links by hovering on it with cursor
+window.addEventListener('mouseover', activeCursor); 
+
+animateSlides()
 
 
 
